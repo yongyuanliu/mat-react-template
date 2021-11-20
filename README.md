61635368bfa3cfb475f9d2fcba8907c6d9cf12a7
Yongyuan Liu <stormbluer@outlook.com>
2021-11-20 09:48:19 +0800

    first push

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mat-react-template
-use the react webpack，template
+> 前端开发模板，基于 react 技术所开发的基础性项目
+
+## Running Locally
+
+### Node
+
+```
+  npm run build
+  npm run start
+```
+
+在本地启动项目 [http://localhost:9000/](http://localhost:9000/)
+
+## WiKi
+
+### entry-01
+  入口文件
+
+  * assets 公共资源文件
+  * components 公共组件
+  * controller 控制器/model
+  * pages 页面具体逻辑
+  * options 一些辅助类的文件
+  * utils 工具类文件
+  
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+/**
+ * module import
+ */
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+/**
+ * const variables init
+ */
+const { NODE_ENV } = process.env;
+const production = 'production', development = 'development';
+const isProduction = Object.is(NODE_ENV, production), isDevelopment = Object.is(NODE_ENV, development);
+console.log(`check NODE_ENV: ${NODE_ENV}, isProduction: ${isProduction}, isDevelopment: ${isDevelopment}`)
+
+/**
+ * webpack dev server config
+ */
+const serverConfig = {
+    compress: true,
+    port: 9000
+}
+
+/**
+ * webpack config mode
+ * @returns ${production} or ${development}
+ * @throws Error
+ */
+function getAndCheckWebpackMode(){
+    if(isProduction){
+        return production;
+    }
+    if(isDevelopment){
+        return development;
+    }
+    throw new Error(`check the NODE_ENV, must be ${production} or ${development} `)
+}
+
+/**
+ * webpack config
+ * entry,output,module,splitChunks...
+ */
+const webpackConfig = {
+    mode: getAndCheckWebpackMode(),
+    entry:{
+        entry01: './entry-01/index.js',
+    },
+    output:{
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name]-bundle.js',
+        clean: true
+    },
+    module:{
+        rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                    options:{
+                        presets:[
+                            '@babel/preset-env', 
+                            {
+                                plugins:['@babel/plugin-transform-runtime']
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.m?css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template: './entry-01/template.html',
+            filename: 'index.html',
+            title: 'Hello World!',
+            chunks: ['entry01']
+        })
+    ],
+    devServer: serverConfig
+};
+
+module.exports = webpackConfig;
